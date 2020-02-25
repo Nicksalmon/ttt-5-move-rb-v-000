6a8b2de9f0c92b16cb00ce4b75b3c13c1c1f3891
Nick Salmon <nickmidspace@gmail.com>
2020-02-25 08:21:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,14 +6,6 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  input.to_i - 1
-end
-
-
-def move(board, position, char="X")
-  board[position]= char
-end
 
 
 
